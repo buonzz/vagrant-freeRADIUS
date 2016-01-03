f104e5624ffc3da29332f9a3f46e98ad534a309b
buonzz <buonzz@gmail.com>
2016-01-03 11:21:33 +0800

    daloRadius

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,3 +8,6 @@ ucf --purge /boot/grub/menu.lst
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get -o Dpkg::Options::="--force-confnew" --force-yes -fuy dist-upgrade
+
+sudo apt-get install php5-common php5-gd php-pear php-db libapache2-mod-php5 php-mail -y
+sudo apt-get install freeradius freeradius-mysql freeradius-utils -y
